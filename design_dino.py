7761dda76655a113c9c150c0c21550ed53376674
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2019-10-26 02:00:25 +0530

    Dino design updated

--- design_dino.py
+++ design_dino.py
@@ -18,4 +18,28 @@ s = space4 + ' ' + top + '\n' + space4 + 3 * block + '\n' + space4 + L +  2 * lo
 #s += space3 + 2 * block + '' + 3 * block + '\n'
 s += space3 + head + 2 * head1 + head + '\n'
 s += space3 + 2 * head1 + ' ' + 2 * head1
+
+part1 = space4 + ' ' + top + '\n' + space4 + 3 * block + '\n' + space4 + L +  2 * lower + '\n'
+part2 = space4 + block + lower + '\n' + space3 + lower +  5 * block + head1 + line + '\n' + knight + 2 * lower2  + 2 * quart + 5 * block + '\n'
+part3_1 = space3 + head + 2 * head1 + head + '\n' + space3 + 2 * head1 + ' ' + 2 * head1
+part3_2 = space2 + head + 4 * head1 + head + '\n' + space2 + 2 * head1 + '   ' + 2 * head1
+lines = 10 * ['']
+lines[0] = space4 + ' ' + top
+lines[1] = space4 + 3 * block
+lines[2] = space4 + L +  2 * lower
+lines[3] = space4 + block + lower
+lines[4] = space3 + lower +  5 * block + head1 + line
+lines[5] = knight + 2 * lower2  + 2 * quart + 5 * block
+lines[6] = space3 + head + 2 * head1 + head
+lines[7] = space3 + 2 * head1 + ' ' + 2 * head1
+lines[8] = space2 + head + 4 * head1 + head
+lines[9] = space2 + 2 * head1 + '   ' + 2 * head1
+s = ""
+for i in range(10):
+	if i == 6 or i == 7:
+		continue
+	s += lines[i] + '\n'
 print(s)
+
+cactus = '\U0001F335'
+print(cactus + '\n' + cactus)
--- dino_trial_v2.py
+++ dino_trial_v2.py
@@ -19,10 +19,10 @@ def viewScores():
 	
 class Game:
 	# Initializes the game configuration
-	def __init__(self, name, dim = terminal, thres = 15, prob = 0.1, col_dino = 3) :
+	def __init__(self, name, dim = terminal, thres = 15, prob = 0.1, col_dino = 6) :
 		self.height = dim[0]
 		self.width = dim[1]
-		self.up = self.height - 4
+		self.up = self.height - 8
 		self.down = self.height - 1
 		self.jump_dir = 0
 		self.r_dino = self.down
@@ -46,9 +46,9 @@ class Game:
 				self.jump_dir = 1
 
 			# updates the game configuration
-			self.score=self.score+1
+			self.score = self.score + 1
 			self.update()
-		   
+		os.system('sleep 2')
 		# update the score
 		os.system('clear')
 		print("Your Score\t" + str(self.score) + "\n")
@@ -65,20 +65,69 @@ class Game:
 			myFile.write(x+ "\t" + str(y) + "\n")
 		myFile.close()
 
+	# unicodes for displaying dinosaur on the terminal
+	# Please do not try to understand the following function and
+	# what does each variable means
+	# for more information : https://en.wikipedia.org/wiki/Block_Elements
+
+	def dino_design(self):
+
+		head = '\U0000259B'
+		head1 = '\U00002580'
+		top = '\U00002582'
+		lower = '\U00002584'
+		quart = '\U00002586'
+		L = '\U00002599'
+		head2 = '\U0000258D'
+		block = '\U00002588'
+		line = '\U0000258C'
+		knight = '\U0000259C'
+		lower2 = '\U00002582'
+		space4 = '       '
+		space3 = '     '
+		space2 = '    '
+		space1 = '   '
+
+		lines = 10 * ['']
+		lines[0] = space4 + ' ' + top
+		lines[1] = space4 + 3 * block
+		lines[2] = space4 + L +  2 * lower
+		lines[3] = space4 + block + lower
+		lines[4] = space3 + lower +  5 * block + head1 + line
+		lines[5] = knight + 2 * lower2  + 2 * quart + 5 * block
+		lines[6] = space3 + head + 2 * head1 + head
+		lines[7] = space3 + 2 * head1 + ' ' + 2 * head1
+		lines[8] = space2 + head + 4 * head1 + head
+		lines[9] = space2 + 2 * head1 + '   ' + 2 * head1
+		if self.score & 1 :
+			return lines[: 8]
+		return lines[: 6] + lines[8 :]
+
 	def display(self):
+
 		# Fill the entire 2D array with 
 		# space characters
 		for i in range(self.height):
 			for j in range(self.width):
 				self.background[i][j] = ' '
-       
+		
+		# dino 'image'
+		# 8 lines of dino 'image'
+		dino = self.dino_design()
+
+		for i in range(8):
+			length = len(dino[8 - i - 1])
+			self.background[self.r_dino - i][: length] = dino[8 - i - 1]
+
 		# Place all the obstacles as '|'
 		# character into the background
+		cactus = '\U0001F335'
 		for col in self.obs:
-			self.background[self.down][col] = '|'
+			self.background[self.down - 1][col] = cactus
+			self.background[self.down][col] = cactus
        
 		# Place the dinosaur into the background
-		self.background[self.r_dino][self.c_dino] = '*'
+		# self.background[self.r_dino][self.c_dino] = '*'
 		# Initialize string to be printed with
 		# empty string
 		s = ""
@@ -94,7 +143,7 @@ class Game:
 		os.system('clear')
 		# Print the string
 		print (s)
-		os.system("sleep 0.06")
+		os.system("sleep 0.1")
    
 	def generateCactus(self):
 		'''
@@ -114,7 +163,7 @@ class Game:
 		for col in self.obs:
 			# Checking whether it Dino touched Cactus or not
 			# if it touches return true indicating game is over
-			if(col == self.c_dino and self.r_dino == self.height - 1):
+			if(abs(col - self.c_dino) <= 3 and self.r_dino >= self.height - 2):
 				return True
 		# Else return false indicating game is not over
 		return False
