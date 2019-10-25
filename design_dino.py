8f0d7e2cf858c0c14d97142660ddfb53fc0e668b
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2019-10-25 15:58:07 +0530

    Dino design created

---
+++ design_dino.py
@@ -0,0 +1,21 @@
+head = '\U0000259B'
+head1 = '\U00002580'
+top = '\U00002582'
+lower = '\U00002584'
+quart = '\U00002586'
+L = '\U00002599'
+head2 = '\U0000258D'
+block = '\U00002588'
+line = '\U0000258C'
+knight = '\U0000259C'
+lower2 = '\U00002582'
+space4 = '       '
+space3 = '     '
+space2 = '    '
+space1 = '   '
+# s = ' ' + top + '\n' + 3 * block + '\n' +  head + 2 * head1 + '\n' + head + 2 * head1
+s = space4 + ' ' + top + '\n' + space4 + 3 * block + '\n' + space4 + L +  2 * lower + '\n' + space4 + block + lower + '\n' + space3 + lower +  5 * block + head1 + line + '\n' + knight + 2 * lower2  + 2 * quart + 5 * block + '\n'
+#s += space3 + 2 * block + '' + 3 * block + '\n'
+s += space3 + head + 2 * head1 + head + '\n'
+s += space3 + 2 * head1 + ' ' + 2 * head1
+print(s)
